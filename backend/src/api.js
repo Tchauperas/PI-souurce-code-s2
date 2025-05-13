799ef692d9894827f230fc0ee9fa366eb3f443a9
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-05-13 09:59:05 -0300

    Pequenas mudanças

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = require("./routers/router");
 const api = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 api.use(cors({
-    origin: "*",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
