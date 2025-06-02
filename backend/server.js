6ac3eb617c2efa276072a4525620f7530f7198dd
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-06-02 07:57:32 -0300

    Commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 const api = require("./src/api");
+const express = require('express')
+const path = require('path')
 
-api.listen(process.env.PORT, () => {
+api.use(express.static(path.join(__dirname, '../frontend')));
+
+api.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+api.listen(process.env.PORT, '0.0.0.0' ,() => {
   console.log(`API is running on port ${process.env.PORT}`);
 });
